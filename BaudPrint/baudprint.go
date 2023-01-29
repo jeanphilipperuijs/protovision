de892c4a6be0fc340debec533cb3181f09b6c15c
Jean-Philippe Ruijs <jean-philippe@ruijs.fr>
2023-01-29 22:01:17 +0100

    baudrate to bps

--- BaudPrint/baudprint.go
+++ BaudPrint/baudprint.go
@@ -7,24 +7,24 @@ import (
 )
 
 func BaudPrint(message string, baudrate int64, variability int, playmp3 bool, playaiff bool) {
-
-	min := int(baudrate) - variability
-	max := int(baudrate) + variability
+	bps := baudrate / 10
+	min := int(bps) - (variability / 10)
+	max := int(bps) + (variability / 10)
 
 	for _, v := range message {
 		rbr := rand.Intn(max-min) + min
 		//fmt.Println("\t", max, min, rbr)
 		time.Sleep(time.Duration(time.Second / time.Duration(rbr)))
-
-		if playmp3 {
-			Playmp3("./BaudPrint/baud.mp3")
-		} else if playaiff {
-			Play("./BaudPrint/baud.aiff")
-		}
-
-		fmt.Printf(string(v))
+		/*
+			if playmp3 {
+				Playmp3("./BaudPrint/baud.mp3")
+			} else if playaiff {
+				Play("./BaudPrint/baud.aiff")
+			}
+		*/
+		fmt.Printf("%c", v)
 	}
 	//time.Sleep(time.Duration(time.Second / 2))
-	time.Sleep(time.Duration(time.Duration(1 / baudrate)))
+	time.Sleep(time.Duration(time.Duration(1 / bps)))
 
 }
